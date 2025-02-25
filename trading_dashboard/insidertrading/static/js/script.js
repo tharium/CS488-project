c91128570f6fc256ddcf875faf09b75280f477ad
Tyler Harris <56047826+tharium@users.noreply.github.com>
2025-02-24 22:27:15 -0800

    Used chart.js to display historical stock data

--- trading_dashboard/insidertrading/static/js/script.js
+++ trading_dashboard/insidertrading/static/js/script.js
@@ -1,4 +1,4 @@
-let stockChart = null;
+var stockChart = null;
 
 function fetchStockPrice() {
     let symbol = document.getElementById("stock-symbol").value;
@@ -35,7 +35,7 @@ function fetchStockHistory(){
             if (data.error) {
                 document.getElementById("result").innerText = "Error: " + data.error;
             } else {
-                document.getElementById("result").innerText = `Stock History for ${data.symbol}:`;
+                document.getElementById("result").innerText = `Stock History for ${symbol}:`;
 
                 displayStockChart(data.dates, data.prices, symbol)
             }
@@ -47,35 +47,28 @@ function fetchStockHistory(){
 }
 
 function displayStockChart(dates, prices, symbol) {
-    let ctx = document.getElementById("stockChart").getContext("2d");
 
-    //removes previous chart if existing
-    if(stockChart) {
+    if (stockChart) {
         stockChart.destroy();
     }
+    
+    let ctx = document.getElementById("stockChart").getContext("2d");
 
-    stockChart = new CharacterData(ctx, {
+    symbol = symbol.toUpperCase();
+
+    stockChart = new Chart(ctx, {
         type: "line",
         data: {
             labels: dates,
             datasets: [{
                 label: `Price of ${symbol}`,
                 data: prices,
-                borderColor: "rgba(75, 192, 192, 1)",
+                borderColor: "rgb(62, 187, 79)",
                 borderWidth: 2,
+                pointBorderWidth: 0,
+                pointRadius: 0,
                 fill: false,
             }]
-        },
-        options: {
-            responsive: true,
-            scales: {
-                x: {
-                    title: {display: true, text: "Date"}
-                },
-                y: {
-                    title: {display: true, text: "Price ($)"},
-                }
-            }
         }
-    })
+    });
 }
