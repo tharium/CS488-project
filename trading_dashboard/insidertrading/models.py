9cd23172f899f508a812004f1e74deb510eedc74
Tyler Harris <56047826+tharium@users.noreply.github.com>
2025-02-26 18:03:21 -0800

    Changed return for stock model

--- trading_dashboard/insidertrading/models.py
+++ trading_dashboard/insidertrading/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Watchlist(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     stocks = models.ManyToManyField('Stock', blank=True)
 
 class Stock(models.Model):
     ticker = models.CharField(max_length=10, unique=True)
+    company_name = models.CharField(max_length=255) 
+    sector = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return self.ticker
+        return f"{self.ticker} - {self.company_name}"
